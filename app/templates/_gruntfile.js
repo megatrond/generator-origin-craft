8cb4bf56e5137d9b466620dbcbaa07c915e56280
Trond Sørås <trond@origin.no>
2015-01-22 12:36:34 +0100

    added cachebusting to build process

--- app/templates/_gruntfile.js
+++ app/templates/_gruntfile.js
@@ -7,6 +7,8 @@ module.exports = function(grunt) {
     var dests = pkg.originBuild.dest;
     var srcs = pkg.originBuild.src;
 
+    var cacheMap = [];
+
     grunt.initConfig({
         sass: {
             dev: {
@@ -22,6 +24,19 @@ module.exports = function(grunt) {
                         ext: '.css'
                     }
                 ]
+            },
+            prod: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: srcs.scss,
+                    src: ['styles.scss'],
+                    dest: dests.css,
+                    ext: '.css'
+                }]
             }
         },
         imagemin: {
@@ -43,6 +58,25 @@ module.exports = function(grunt) {
                 dest: dests.js+'/bundle.js'
             }
         },
+        uglify: {
+            options: {
+                compress: {
+                    drop_console: true
+                }
+            },
+            prod: {
+                files: {
+                    'public/static/js/bundle.js': ['public/static/js/bundle.js']
+                }
+            }
+        },
+        cssmin: {
+            prod: {
+                files: {
+                    'public/static/css/styles.min.css': ['public/static/css/styles.css']
+                }
+            }
+        },
         copy: {
             dev: {
                 files: [
@@ -51,6 +85,16 @@ module.exports = function(grunt) {
                         cwd: 'src',
                         src: 'fonts/**/*',
                         dest: 'public/static',
+                        filter: 'isFile'
+                    }
+                ]
+            },
+            prod: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['craft/**/*', 'public/static/**/*', 'public/*', '!public/uploads'],
+                        dest: 'release/latest',
                         filter: 'isFile'
                     }
                 ]
@@ -65,7 +109,8 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        src: ['craft/**/*', 'public/static/**/*', 'public/*', '!public/uploads'],
+                        cwd: 'release/latest',
+                        src: ['**/*'],
                         dest: pkg.name
                     }
                 ]
@@ -82,13 +127,12 @@ module.exports = function(grunt) {
                             replacement: map[file]
                         });
                     });
-                    console.log(cacheMap);
                 }
             },
             prod: {
                 files: [{
                     expand: true,
-                    cwd: 'public/',
+                    cwd: 'release/latest/public/',
                     src: ['static/css/styles.css', 'static/js/bundle.js'],
                     dest: '.'
                 }]
@@ -98,14 +142,24 @@ module.exports = function(grunt) {
             prod: {
                 files:[{
                     expand: true,
-                    cwd: 'craft/templates/common',
+                    cwd: 'release/latest/craft/templates/common',
                     src: ['doc_head.twig', 'js.twig'],
-                    dest: 'craft/templates/common'
+                    dest: 'release/latest/craft/templates/common'
                 }],
                 options: {
                     replacements: cacheMap
                 }
             }
+        },
+        clean: {
+            prod: [
+                'release/latest/public/static/css/styles.css',
+                'release/latest/public/static/css/styles.css.map',
+                'release/latest/public/static/js/bundle.js'
+            ],
+            postprod: [
+                'release/latest'
+            ]
         },
         watch: {
             css: {
@@ -141,7 +195,10 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.registerTask('cachebust', ['bushcaster:prod', 'string-replace:prod']);
+
+
     grunt.registerTask('default', ['copy:dev', 'sass:dev', 'imagemin:dev', 'browserify:dev', 'watch']);
 
-    grunt.registerTask('release', ['copy:dev', 'sass:prod', 'imagemin:dev', 'browserify:dev', 'uglify:prod', 'compress:release']);
+    grunt.registerTask('release', ['copy:dev', 'sass:prod', 'imagemin:dev', 'browserify:dev', 'uglify:prod','copy:prod', 'bushcaster:prod', 'string-replace:prod', 'clean:prod', 'compress:release', 'clean:postprod']);
 };
